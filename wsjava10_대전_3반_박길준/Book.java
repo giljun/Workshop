572f7f09aed10644b80761be9beae5a5eaa4ed2d
박길준 <adda281@naver.com>
2019-01-23 10:58:50 +0000

    2019.01.23 wsjava10_대전_3_박길준 Book.java

---
+++ wsjava10_대전_3반_박길준/Book.java
@@ -0,0 +1,57 @@
+package com.ssafy;
+
+public class Book {
+	protected String isbn;
+	protected String title;
+	protected int price;
+	protected int quantity;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String isbn, String title, int price, int quantity) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return isbn + "|" + title + "|" + price + "|" + quantity;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}

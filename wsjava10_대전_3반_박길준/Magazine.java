60cf80ac3af53d0987f2b0adfb2db775220dca31
박길준 <adda281@naver.com>
2019-01-23 11:01:21 +0000

    2019.01.23 wsjava10_대전_3_박길준 Magazine.java

---
+++ wsjava10_대전_3반_박길준/Magazine.java
@@ -0,0 +1,28 @@
+package com.ssafy;
+
+public class Magazine extends Book {
+	private int month;
+
+	public Magazine() {
+		super();
+	}
+
+	public Magazine(String isbn, String title, int price, int quantity, int month) {
+		super(isbn, title, price, quantity);
+		this.month = month;
+	}
+
+	@Override
+	public String toString() {
+		return isbn + "|" + title + "|" + price + "|" + quantity + "|" + month;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+}
